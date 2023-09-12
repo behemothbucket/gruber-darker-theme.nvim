bde11f22be0b3abaca1bc9200b7653a0f82402c0
1option <mahagon.here@gmail.com>
2023-09-12 23:20:24 +0300

    update

--- lua/gruber-darker/init.lua
+++ lua/gruber-darker/init.lua
@@ -111,7 +111,7 @@ local highlight_groups = {
   ["javaScriptIdentifier"]   = { link = "Keyword" },
   ["javaScriptGlobal"]       = { link = "Keyword" },
   ["javaScriptMember"]       = { link = "Function" },
-  ["javaScriptStatement"]    = { fg = p.gruber_darker_yellow },
+  ["javaScriptStatement"]    = { fg = p.gruber_darker_yellow_custom },
   ["markdownCodeBlock"]      = { link = "String" },
   ["markdownCodeDelimiter"]  = { link = "Keyword" },
   -- Diff
