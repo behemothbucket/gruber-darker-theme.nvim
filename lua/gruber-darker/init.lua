7a010060a38e1a3230f1898ca35f4a6b3043a17f
1option <mahagon.here@gmail.com>
2023-09-13 11:56:27 +0300

    update

--- lua/gruber-darker/init.lua
+++ lua/gruber-darker/init.lua
@@ -115,7 +115,7 @@ local highlight_groups = {
   ["javaStatement"]                                   = { link = "Keyword" },
   ["javaOperator"]                                    = { link = "Keyword" },
   ["@lsp.type.property.java"]                         = { fg = p.gruber_darker_quartz },
-  ["@lsp.mod.typeArgument.java"]                      = { fg = p.gruber_darker_quartz },
+  -- ["@lsp.mod.typeArgument.java"]                      = { fg = p.gruber_darker_quartz },
   ["@lsp.typemod.class.readonly.java"]                = { fg = p.gruber_darker_quartz },
   -- JavaScript
   ["javaScriptIdentifier"]                            = { link = "Keyword" },
